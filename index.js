61ddc78746a7d392360c6295c7b34c832bcf2c6f
Harry Nhu <harry.nhu@vmodev.com>
2022-11-07 11:07:42 +0700

    feat: update models and routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
     console.log("Connection failed " + err);
   });
 
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ models/Products.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+    size: {
+      type: Array,
+    },
+    color: {
+      type: string,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
